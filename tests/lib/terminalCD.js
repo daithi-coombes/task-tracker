362fa3408be1a606d9bfbffef06ebb7d2f2bd008
Daithi Coombes <webeire@gmail.com>
2015-10-08 12:35:25 +0100

    parse data concept

---
+++ tests/lib/terminalCD.js
@@ -0,0 +1,10 @@
+var rewire = require('rewire')
+  , terminalCD = rewire('../../lib/terminalCD')
+
+describe('Should parse directory changes in the terminal', function(){
+
+  it('Should open the terminal-cd log file', function(){
+
+
+  })
+})
