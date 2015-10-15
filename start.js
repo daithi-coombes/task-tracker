fcac2edae274febc43b3810a89ec582f5b3841fb
Daithi Coombes <webeire@gmail.com>
2015-10-15 10:40:28 +0100

    read lines from log file
    Signed-off-by: Daithi Coombes <webeire@gmail.com>

---
+++ start.js
@@ -0,0 +1,5 @@
+var terminalCD = require('./lib/terminalCD')
+
+terminalCD.readLog(null, function(err, data){
+  console.log(data)
+})
--- tests/lib/terminalCD.js
+++ tests/lib/terminalCD.js
@@ -1,10 +1,51 @@
-var rewire = require('rewire')
+var _ = require('lodash')
+  , rewire = require('rewire')
+  , mockFs = require('mock-fs')
   , terminalCD = rewire('../../lib/terminalCD')
 
-describe('Should parse directory changes in the terminal', function(){
+describe('Interact with local log file', function(){
 
-  it('Should open the terminal-cd log file', function(){
+  it('Should read lines from terminal-cd', function(done){
 
+    var expected = [{
+        dateTime: '2015-10-15T08:35:46,507586706+0000',
+        cmd: 'cd ../taskTracker/'
+      },{
+        dateTime: '2015-10-15T08:35:46,503218838+0000',
+        cmd: 'cd /opt/coder-forge'
+    }]
+
+    mockFs({
+      '/var/log/': {
+        'terminal-cd.log': expected[0].dateTime+' '+expected[0].cmd+' \n'+expected[1].dateTime+' '+expected[1].cmd+' \n' //spaces test trim()
+      }
+    })
+
+    terminalCD.readLog(null, function(err, lines){
+      mockFs.restore()
+
+      for(var x=0; x<expected.length; x++)
+        if(!_.isEqual(expected[x], lines[x])){
+          console.log(expected[x])
+          console.log(lines[x])
+          return false;
+        }
+
+      mockFs.restore()
+      done()
+    })
+  })
+
+  it('Should truncate local log file', function(){
+    terminalCD.truncate(null, function(){
+
+    })
+  })
+})
+
+describe('Interact with Loggly API', function(){
+
+  it('Should send line to loggly', function(){
 
   })
 })
