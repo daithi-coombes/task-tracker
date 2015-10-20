ff490d4c22b118482c379e61e7510432b214bb04
Daithi Coombes <webeire@gmail.com>
2015-10-20 14:50:31 +0100

    add express framework

--- start.js
+++ start.js
@@ -8,11 +8,17 @@ logger.setConfig(config)
 terminalCD.readLog(null, function(err, data){
 
   for(var x=0; x<data.length; x++)
+
+    terminalCD.writeLn(data[x], function(err, result){
+      
+    })
+
+    /*
     logger.log(data[x], function(err, result){
       console.log(err)
       console.log(result)
       console.log('**************')
-    })
+    })**/
 
   terminalCD.backupLog(null, function(err, backupFile){
     if(err)
