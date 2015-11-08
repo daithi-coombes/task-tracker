9f1ff674a65ab8cc708bed93dfca226450a2155e
Daithi Coombes <webeire@gmail.com>
2015-11-08 17:05:48 +0000

    meteor skeleton

--- bin/startup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# note: this is my own personal script for setting up 2nd monitor and running
-# tmux. Not part of the taskTraker package.
-
-xrandr --output VGA1 --right-of LVDS1
-tmux
