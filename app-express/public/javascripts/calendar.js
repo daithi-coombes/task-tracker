63d2eb4292f7f217219f5015ba8ad4e38c1bbfd9
Daithi Coombes <webeire@gmail.com>
2015-11-18 16:45:03 +0000

    get user inputed start/end dateTimes
    Signed-off-by: Daithi Coombes <webeire@gmail.com>

--- app-express/public/javascripts/calendar.js
+++ app-express/public/javascripts/calendar.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+
+  /**
+   * Load fullcalendar.
+   */
   $('#calendar').fullCalendar({
     header: {
       left: 'prev,next today',
@@ -8,41 +12,59 @@ $(document).ready(function(){
     },
     selectable: true,
     select: function(start, end, allDay, ev){
-      console.log(arguments);
+
+      var s = start._d,
+        e = end._d;
+
+      var startStr = s.getDate()+'/'+(s.getMonth()+1)+'/'+s.getFullYear()+' '+s.getHours()+':'+('0'+s.getMinutes()).slice(-2);
+      var endStr = e.getDate()+'/'+(e.getMonth()+1)+'/'+e.getFullYear()+' '+e.getHours()+':'+('0'+e.getMinutes()).slice(-2);
+      $('input[name=startDateTime]').val(startStr);
+      $('input[name=endDateTime]').val(endStr);
+      $('#taskModal').modal();
     },
-    /**dayClick: function(date, jsEvent, view){
-      console.log(arguments);
-    },*/
     defaultView: 'agendaWeek'
   });
+  //end Load fullcalendar
+
+
+  var opts = {
+    format: 'DD/MM/YYYY h:mm'
+  };
+  $('#startDateTime').datetimepicker(opts);
+  $('#endDateTime').datetimepicker(opts);
 
 })
 
+
+/**
+ * Drag selection box.
+ */
 $(document)
-.drag("start",function( ev, dd ){
-  return $('<div class="selection" />')
-    .css('opacity', .65 )
-    .appendTo( document.body );
-})
-.drag(function( ev, dd ){
-  $( dd.proxy ).css({
-    top: Math.min( ev.pageY, dd.startY ),
-    left: Math.min( ev.pageX, dd.startX ),
-    height: Math.abs( ev.pageY - dd.startY ),
-    width: Math.abs( ev.pageX - dd.startX )
+  .drag("start",function( ev, dd ){
+    return $('<div class="selection" />')
+      .css('opacity', .65 )
+      .appendTo( document.body );
+  })
+  .drag(function( ev, dd ){
+    $( dd.proxy ).css({
+      top: Math.min( ev.pageY, dd.startY ),
+      left: Math.min( ev.pageX, dd.startX ),
+      height: Math.abs( ev.pageY - dd.startY ),
+      width: Math.abs( ev.pageX - dd.startX )
+    });
+  })
+  .drag("end",function( ev, dd ){
+    $( dd.proxy ).remove();
   });
-})
-.drag("end",function( ev, dd ){
-  $( dd.proxy ).remove();
-});
-$('.drop')
-.drop("start",function(){
-  $( this ).addClass("active");
-})
-.drop(function( ev, dd ){
-  $( this ).toggleClass("dropped");
-})
-.drop("end",function(){
-  $( this ).removeClass("active");
-});
-$.drop({ multi: true });
+  $('.drop')
+  .drop("start",function(){
+    $( this ).addClass("active");
+  })
+  .drop(function( ev, dd ){
+    $( this ).toggleClass("dropped");
+  })
+  .drop("end",function(){
+    $( this ).removeClass("active");
+  });
+  $.drop({ multi: true });
+//end Drag selection box
